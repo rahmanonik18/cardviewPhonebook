2f3028700038a31b1e5dfc5272b9626d3ad97f19
rahman <abduronikrahman@gmail.com>
2021-05-08 07:36:24 +0600

    Cardview Phonebook

---
+++ app/src/main/java/com/tahmidrasif/phonebook/Activity/MainActivity.java
@@ -0,0 +1,65 @@
+package com.tahmidrasif.phonebook.Activity;
+
+import android.content.Context;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.tahmidrasif.phonebook.Adapter.CustomAdapter;
+import com.tahmidrasif.phonebook.Model.DataModel;
+
+import java.util.ArrayList;
+
+import com.tahmidrasif.phonebook.Model.MyData;
+import com.tahmidrasif.phonebook.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private static RecyclerView recyclerView;
+    private static ArrayList<DataModel> data;
+    public static View.OnClickListener myOnClickListener;
+    private static ArrayList<Integer> removedItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //myOnClickListener = new MyOnClickListener(this);
+
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        data = new ArrayList<DataModel>();
+        Log.e("mydatasize==>", String.valueOf(MyData.nameArray.length));
+
+        for (int i = 0; i < MyData.nameArray.length; i++) {
+            data.add(new DataModel(
+                    MyData.nameArray[i],
+                    MyData.versionArray[i],
+                    MyData.id_[i],
+                    MyData.drawableArray[i]
+            ));
+            Log.e("printdata==>", String.valueOf(data));
+        }
+        adapter = new CustomAdapter(data);
+        recyclerView.setAdapter(adapter);
+    }
+
+}
---
+++ app/src/main/java/com/tahmidrasif/phonebook/Model/MyData.java
@@ -0,0 +1,15 @@
+package com.tahmidrasif.phonebook.Model;
+
+
+import com.tahmidrasif.phonebook.R;
+
+
+public class MyData {
+
+   public static String[]nameArray = {"Rasif Tahmid Islam", "Tahmid Rasif", "Islam Rasif",};
+   public static String[] versionArray = {"017", "017", "01"};
+
+     public static Integer[] drawableArray = {R.drawable.rasif, R.drawable.rasif, R.drawable.rasif};
+
+   public static Integer[] id_ = {0, 1, 2, 3};
+}
